a2689567ec003f32f7835238f5747c7737b69763
Jaiahir <jbahir@ualr.edu>
2022-01-27 19:25:19 +0000

    1/26/22

--- 01.05 - Previous and Next.py
+++ 01.05 - Previous and Next.py
@@ -1,6 +1,4 @@
 x = input("Enter a number: ")
 number = int(x)
-fnumber = number+1
-pnumber = number-1
-print("The next number for the number " + number + "is " + fnumber + ".")
-print("The previous number for the number " + number + "is " + pnumber + ".")
+print("The next number for the number " + str(number) + " is " + str(number+1) + ".") 
+print("The previous number for the number " + str(number) + " is " + str(number-1) + ".")
