7c9026d2a77a3887f4c96fc68275ab550dd49319
Jaiahir <jbahir@ualr.edu>
2022-02-09 16:36:47 +0000

    2/9/22

--- 03.11 - Project - Unit Conversion.py
+++ 03.11 - Project - Unit Conversion.py
@@ -1,27 +1,73 @@
 FromValue = float(input("Enter From Value: "))
-FromUnit = str(input("Enter From Unit (in, ft, yd, mi): "))
-ToUnit = str(input("Enter To Unit (in, ft, yd, mi): "))
 
-miToin = FromUnit * 63360
-miToft = FromUnit * 5280
-miToyd = FromUnit * 1760
+inch = "inch"
+ft = "ft"
+yd = "yd"
+mi = "mi"
 
-ydToin = FromUnit * 36
-ydToft = FromUnit * 3
-ydTomi = FromUnit * 0.0005681818181818
+FromUnit = str(input("Enter From Unit (inch, ft, yd, mi): "))
+if FromUnit == inch or ft or yd or mi:
+    print("")
+else:
+    quit("FromUnit is not valid")
 
-ftToin = FromUnit * 12
-ftToyd = FromUnit * 0.3333333333
-ftTomi = FromUnit * 0.0001893939393939
 
-inToft = FromUnit * 0.08333333333333
-inToyd = FromUnit * 0.02777777777778
-inTomi = FromUnit * 0.00001578282828283
+ToUnit = str(input("Enter To Unit (inch, ft, yd, mi): "))
+if ToUnit == inch or ft or yd or mi:
+    print("")
+else:
+    quit("ToUnit is not valid")
 
-inToin = FromUnit * 1
-ftToft = FromUnit * 1
-ydToyd = FromUnit * 1
-miTomi = FromUnit * 1
+miToin = FromValue * 63360
+miToft = FromValue * 5280
+miToyd = FromValue * 1760
 
-if FromUnit == mi and ToUnit == in:
-    print(miToin)
+ydToin = FromValue * 36
+ydToft = FromValue * 3
+ydTomi = FromValue * 0.0005681818181818
+
+ftToin = FromValue * 12
+ftToyd = FromValue * 0.3333333333
+ftTomi = FromValue * 0.0001893939393939
+
+inToft = FromValue * 0.08333333333333
+inToyd = FromValue * 0.02777777777778
+inTomi = FromValue * 0.00001578282828283
+
+inToin = FromValue * 1
+ftToft = FromValue * 1
+ydToyd = FromValue * 1
+miTomi = FromValue * 1
+
+if FromUnit == mi and ToUnit == inch:
+    print(round(miToin,7), "in")
+elif FromUnit == mi and ToUnit == ft:
+    print(round(miToft,7), "ft")
+elif FromUnit == mi and ToUnit == yd:
+    print(round(miToyd,7), "yd")
+elif FromUnit == mi and ToUnit == mi:
+    print(round(miTomi,7), "mi")
+elif FromUnit == ft and ToUnit == inch:
+    print(round(ftToin,7), "in")
+elif FromUnit == ft and ToUnit == ft:
+    print(round(ftToft,7), "ft")
+elif FromUnit == ft and ToUnit == yd:
+    print(round(ftToyd,7), "yd")
+elif FromUnit == ft and ToUnit == mi:
+    print(round(ftTomi,7), "mi")
+elif FromUnit == yd and ToUnit == inch:
+    print(round(ydToin,7), "in")
+elif FromUnit == yd and ToUnit == ft:
+    print(round(ydToft,7), "ft")
+elif FromUnit == yd and ToUnit == yd:
+    print(round(ydToyd,7), "yd")
+elif FromUnit == yd and ToUnit == mi:
+    print(round(ydTomi,7), "mi")
+elif FromUnit == inch and ToUnit == inch:
+    print(round(inToin,7), "in")
+elif FromUnit == inch and ToUnit == ft:
+    print(round(inToft,7), "ft")
+elif FromUnit == inch and ToUnit == yd:
+    print(round(inToyd,7), "yd")
+elif FromUnit == inch and ToUnit == mi:
+    print(round(inTomi,7), "mi")
