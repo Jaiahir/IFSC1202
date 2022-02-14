4b040f27750536f9ea4cde2759127417882d76cf
Jaiahir <jbahir@ualr.edu>
2022-02-14 23:35:39 +0000

    2/14/22

---
+++ 04.04 - Number Of Zeros.py
@@ -0,0 +1,10 @@
+x = int(input("Enter N: "))
+
+zero = 0
+
+for i in range(x):
+    if int(input("Enter Number: ")) == 0:
+        zero += 1
+
+print(zero)
+
--- 04.05 - Sum Of Factorials.py
+++ 04.05 - Sum Of Factorials.py
@@ -2,7 +2,10 @@ x = int(input("Enter Number: "))
 
 
 product = 1
+sum = 0
 for i in range(1, x+1):
     product *= i
+    sum += product
 
-print(product)
+
+print(sum)
