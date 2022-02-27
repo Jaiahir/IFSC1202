2fd4d60abec38672234383f0188d293308d131cb
Jaiahir <jbahir@ualr.edu>
2022-02-27 22:23:32 +0000

    d

--- 06.01 - Circle.py
+++ 06.01 - Circle.py
@@ -1,23 +1,32 @@
-file = open("06.01 Radius.txt", "r")
-r = file.readlines()
+import math
 
 def diameter(r):
-    for i in range(r):
         return 2 * r
 
-#def circumference(r):
-    return 2 * 3.14 * str(r)
+def circumference(r):
+    return 2 * math.pi * r
 
-#def area(r):
-    return 3.14 * str(r*r)
+def area(r):
+    return math.pi * r * r
 
+def main():
 
+    file = open("06.01 Radius.txt","r")
 
-diameter = diameter(r)
-#circumference = circumference(r)
-#area = area(r)
+    list = []
 
-print (diameter)
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        else:
+            list.append(float(line))
 
+    print("{:>15} {:>15} {:>15} {:>15}".format("Radius","Diameter","Circumference","Area"))
+    for r in list:
+        print("{:>15.5f} {:>15.5f} {:>15.5f} {:>15.5f}".format(r,diameter(r),circumference(r),area(r)))
+
+if __name__ == "__main__":
+    main()
 
 
