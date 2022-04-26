b8f778ee8eefb1bb047f44406910a421da3a2ece
Jaiahir <jbahir@ualr.edu>
2022-04-26 20:06:49 +0000

    4/26/22

--- 11.03 - Project - Registration.py
+++ 11.03 - Project - Registration.py
@@ -1,84 +1,104 @@
-from tabulate import tabulate
-class Student :
-   def __init__(self, firstname="Mary", lastname="Drake", tnumber="T123567") :
-       self.FirstName = firstname
-       self.LastName = lastname
-       self.TNumber = tnumber
-       self.CourseList = []
+#I did all the directions but couldn't figure out how to pass the commas on every file and got stuck. This means the program won't run but I did do all the instructions.
+def read_from_file(filename):
+    f = open(filename, 'r')
+    lines = f.read().split('\n')
+    lines = list(filter(None, lines))
+    f.close()
+    return lines
 
-class Studentlist :
-   def __init__(self) :
-       self.Studentlist = []
-   def add_student(self, firstname, lastname, tnumber) :
-       student = Student(firstname, lastname, tnumber)
-       self.Studentlist.append(student)
-   def find_student(self, studenttofind) :
-       for i in range(len(self.Studentlist)) :
-           if(self.Studentlist[i].TNumber == studenttofind) :
-               return i
-   def print_student_list(self) :
-       headers = ["First Name", "Last Name", "T-Number", "Course", "Name", "Room", "Meeting Times"]
-       table = [headers]
-       for student in self.Studentlist :
-           studentinfo = [student.FirstName, student.LastName, student.TNumber]
-           table.append(studentinfo)
-           for course in student.CourseList :
+class Student:
+    def __init__(self, firstname, lastname, tnumber):
+        self.FirstName = firstname
+        self.LastName = lastname
+        self.TNumber = tnumber
+        self.CourseList = []
+        return
 
-#For formatting the table as given in output
-               courseinfo = [" "," "," "]
-               courseinfo.extend([course.department+" "+course.number, course.name, course.room, course.meetingtimes])
-               table.append(courseinfo)
-       print(tabulate(table))
+class StudentList():
+    def __init__(self):
+        self.StudentList = []
+        return
 
-   def add_student_from_file(self, filename) :
-       f = open(filename, "r")
-       students = f.readlines()
-       for student in students :
-           student = student.split(',')
-           self.add_student(student[0].strip(), student[1].strip(), student[2].strip())
+    def add_student(self, firstname, lastname, tnumber):
+        newStudent = Student(firstname,lastname,tnumber)
+        self.StudentList.append(newStudent)
+        return
 
-class Course :
-   def __init__(self, department="IFSC", number="1110", name="Introduction to Ethics", room="EIT 211", meetingtimes="M-W 9:00-10:50") :
-       self.department = department
-       self.number = number
-       self.name = name
-       self.room = room
-       self.meetingtimes = meetingtimes
+    def find_student(self, studenttofind):
+        for ind,student in enumerate(self.StudentList):
+            if student.TNumber == studenttofind:
+                return ind
+        return
 
-class Courselist :
-   def __init__(self) :
-       self.Courselist = []
-   def add_course(self, department, number, name, room, meetingtimes) :
-       course = Course(department, number, name, room, meetingtimes)
-       self.Courselist.append(course)
-   def find_course(self, departmenttofind, numbertofind) :
-       for i in range(len(self.Courselist)) :
-           if(self.Courselist[i].department == departmenttofind and self.Courselist[i].number == numbertofind) :
-               return i
-   def add_course_from_file(self, filename) :
-       f = open(filename, "r")
-       courses = f.readlines()
-       for course in courses :
-           course = course.split(',')
-           self.add_course(course[0].strip(), course[1].strip(), course[2].strip(), course[3].strip(), course[4].strip())
+    def print_student_list(self):
+        for student in self.StudentList:
+            print(f'Courses for {student.FirstName} {student.LastName} (T-Number: {student.TNumber})')
+            for course in student.CourseList:
+                print(f'  {course.department}{course.number} - {course.name}')
+            print()
+        return
 
-if __name__ == '__main__' :
-   mycourselist = Courselist()
-   mycourselist.add_course_from_file("11.03 Courses.txt")
-  
-   mystudentlist = Studentlist()
-   mystudentlist.add_student_from_file("11.03 Students.txt")
-  
-   fRegisterations = open("11.03 Registration.txt", "r")
-   registerations = fRegisterations.readlines()
-   for registeration in registerations :
-       registeration = registeration.split(',')
-       department, number = registeration[1].strip(), registeration[2].strip()
-       courseindex = mycourselist.find_course(department, number)
-       if courseindex == None:
-           continue
-       mycourse = mycourselist.Courselist[courseindex]
-       tnumber = registeration[0]
-       studentindex = mystudentlist.find_student(tnumber)
-       mystudentlist.Studentlist[studentindex].CourseList.append(mycourse)
-   mystudentlist.print_student_list()
+    def add_student_from_file(self, filename):
+        students = read_from_file(filename)
+        for student in students:
+            firstname, lastname, tnumber = student.split(',')
+            self.add_student(firstname, lastname, tnumber)
+        return
+
+class Course:
+    def __init__(self, department, number, name, room, meetingtimes):
+        self.department = department
+        self.number = number
+        self.name = name
+        self.room = room
+        self.meetingtimes = meetingtimes
+        return
+
+class CourseList:
+    def __init__(self):
+        self.Courselist = []
+        return
+
+    def add_course(self, department, number, name, room, meetingtimes):
+        newCourse = Course(department, number, name, room, meetingtimes)
+        self.Courselist.append(newCourse)
+        return
+
+    def find_course(self, departmenttofind, numbertofind):
+        for ind,course in enumerate(self.Courselist):
+            if course.department == departmenttofind and course.number == numbertofind:
+                return ind
+        return
+
+    def add_course_from_file(self, filename):
+        courses = read_from_file(filename)
+        print(len(courses))
+        for course in courses:
+            print(len(course.split(',')))
+            department, number, name, room, meetingtimes = course.split(',')
+            self.add_course(department, number, name, room, meetingtimes)
+        return
+
+
+coursesf = '11.03 Courses.txt'
+students = '11.03 Students.txt'
+registration = '11.03 Registration.txt'
+
+mycourselist = CourseList()
+mycourselist.add_course_from_file(coursesf)
+mystudentlist = StudentList()
+mystudentlist.add_student_from_file(students)
+
+
+registration_data = read_from_file(registration)
+for reg in registration_data:
+    tnumber, department, number = reg.split(',')
+    course_index = mycourselist.find_course(department, number)
+    mycourse = mycourselist.Courselist[course_index]
+    mydepartment, mynumber, myname, myroom, mymeetingtimes = mycourse.department, mycourse.number, mycourse.name, mycourse.room, mycourse.meetingtimes
+    mycourse = Course(mydepartment, mynumber, myname, myroom, mymeetingtimes)
+    student_index = mystudentlist.find_student(tnumber)
+    mystudent = mystudentlist.StudentList[student_index]
+    mystudent.CourseList.append(mycourse)
+
+mystudentlist.print_student_list()
